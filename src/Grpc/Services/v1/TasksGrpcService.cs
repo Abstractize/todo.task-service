27a17baab71129ddb2b379defd1814c24b56d632
Abstractize <gababarcaa@gmail.com>
2025-07-22 01:34:40 -0600

    Getting User form jwt

--- src/Grpc/Services/v1/TasksGrpcService.cs
+++ src/Grpc/Services/v1/TasksGrpcService.cs
@@ -2,21 +2,24 @@ using Grpc.Core;
 using Analytics.Protos;
 using Managers.Contracts;
 using Microsoft.AspNetCore.Authorization;
+using Services.Common.Identity;
 
 namespace Grpc.Services.v1;
 
 [Authorize]
-public class TasksGrpcService(ITaskItemManager taskItemManager, ITaskLogManager taskLogManager) : TaskAnalyticsService.TaskAnalyticsServiceBase
+public class TasksGrpcService(ITaskItemManager taskItemManager, ITaskLogManager taskLogManager, IIdentityService identityService) : TaskAnalyticsService.TaskAnalyticsServiceBase
 {
+    private readonly IIdentityService _identityService = identityService;
     private readonly ITaskItemManager _taskItemManager = taskItemManager;
     private readonly ITaskLogManager _taskLogManager = taskLogManager;
 
     public override async Task<TaskList> GetUserTasks(GetTasksRequest request, ServerCallContext context)
     {
-        Guid userId = Guid.Parse(request.UserId);
+        Guid userId = _identityService.UserId ??
+            throw new RpcException(new Status(StatusCode.Unauthenticated, "User is not authenticated"));
         IEnumerable<Models.TaskItem> tasks = await _taskItemManager.GetAsync(userId);
 
-        var taskList = new TaskList();
+        TaskList taskList = new();
         taskList.Tasks.AddRange(tasks.Select(t => new TaskItem
         {
             Id = t.Id.ToString(),
@@ -29,10 +32,9 @@ public class TasksGrpcService(ITaskItemManager taskItemManager, ITaskLogManager 
 
     public override async Task<TaskList> GetUserTasksOfWeek(GetWeeklyTasksRequest request, ServerCallContext context)
     {
-        Guid userId = Guid.Parse(request.UserId);
-        DateTime weekStart = DateTime.Parse(request.WeekStartDateUtc);
-
-        Console.WriteLine($"Fetching tasks for user {userId} starting from week beginning {weekStart.ToUniversalTime().ToString("o")}");
+        Guid userId = _identityService.UserId ??
+            throw new RpcException(new Status(StatusCode.Unauthenticated, "User is not authenticated"));
+        DateTime weekStart = DateTime.Parse(request.DayUtc);
 
         IEnumerable<Models.TaskItem> tasks = await _taskItemManager.GetWeeklyTasksAsync(userId, weekStart);
 
@@ -50,11 +52,6 @@ public class TasksGrpcService(ITaskItemManager taskItemManager, ITaskLogManager 
             FirstActivity = (await _taskLogManager.GetFirstActivityAsync(userId)).ExecutionAtUtc.ToUniversalTime().ToString("o"),
             LastActivity = (await _taskLogManager.GetLastActivityAsync(userId)).ExecutionAtUtc.ToUniversalTime().ToString("o")
         };
-
-        foreach (var task in taskList.Tasks)
-        {
-            Console.WriteLine($"Task ID: {task.Id}, Title: {task.Title}, Completed: {task.IsCompleted}, Created At: {task.CreatedAt}");
-        }
 
         return taskList;
     }
